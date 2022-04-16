2fd92b5d890662887831ff674405674f7af3f051
Zollern Wolf <alphawolf918@users.noreply.github.com>
2022-04-16 10:50:51 -0400

    Initial Commit

---
+++ PassGen1.Designer.cs
@@ -0,0 +1,202 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace XCrypt {
+	partial class frmMain {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.lblBase = new System.Windows.Forms.Label();
+            this.chkUseFirst = new System.Windows.Forms.CheckBox();
+            this.lblNumFirst = new System.Windows.Forms.Label();
+            this.txtNumFirst = new System.Windows.Forms.TextBox();
+            this.chkUseSymb = new System.Windows.Forms.CheckBox();
+            this.lblPassLen = new System.Windows.Forms.Label();
+            this.txtPassLen = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.SystemColors.Window;
+            this.txtPass.Location = new System.Drawing.Point(156, 230);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.ReadOnly = true;
+            this.txtPass.Size = new System.Drawing.Size(342, 40);
+            this.txtPass.TabIndex = 0;
+            this.txtPass.WordWrap = false;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(153, 211);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(97, 20);
+            this.lblPass.TabIndex = 1;
+            this.lblPass.Text = "Password:";
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(259, 167);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(116, 37);
+            this.btnGen.TabIndex = 1;
+            this.btnGen.Text = "Generate";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // txtBase
+            // 
+            this.txtBase.BackColor = System.Drawing.SystemColors.Window;
+            this.txtBase.Location = new System.Drawing.Point(156, 111);
+            this.txtBase.Multiline = true;
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(342, 40);
+            this.txtBase.TabIndex = 0;
+            this.txtBase.WordWrap = false;
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.Location = new System.Drawing.Point(153, 92);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(58, 20);
+            this.lblBase.TabIndex = 4;
+            this.lblBase.Text = "Base:";
+            // 
+            // chkUseFirst
+            // 
+            this.chkUseFirst.AutoSize = true;
+            this.chkUseFirst.Location = new System.Drawing.Point(23, 20);
+            this.chkUseFirst.Name = "chkUseFirst";
+            this.chkUseFirst.Size = new System.Drawing.Size(185, 24);
+            this.chkUseFirst.TabIndex = 5;
+            this.chkUseFirst.Text = "Use Base As Start";
+            this.chkUseFirst.UseVisualStyleBackColor = true;
+            // 
+            // lblNumFirst
+            // 
+            this.lblNumFirst.AutoSize = true;
+            this.lblNumFirst.Location = new System.Drawing.Point(23, 47);
+            this.lblNumFirst.Name = "lblNumFirst";
+            this.lblNumFirst.Size = new System.Drawing.Size(208, 20);
+            this.lblNumFirst.TabIndex = 6;
+            this.lblNumFirst.Text = "Numbers of Base First:";
+            // 
+            // txtNumFirst
+            // 
+            this.txtNumFirst.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNumFirst.Location = new System.Drawing.Point(237, 44);
+            this.txtNumFirst.MaxLength = 1;
+            this.txtNumFirst.Name = "txtNumFirst";
+            this.txtNumFirst.Size = new System.Drawing.Size(35, 27);
+            this.txtNumFirst.TabIndex = 7;
+            this.txtNumFirst.Text = "2";
+            this.txtNumFirst.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNumFirst.WordWrap = false;
+            this.txtNumFirst.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNumFirst_KeyDown);
+            // 
+            // chkUseSymb
+            // 
+            this.chkUseSymb.AutoSize = true;
+            this.chkUseSymb.Checked = true;
+            this.chkUseSymb.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkUseSymb.Location = new System.Drawing.Point(459, 20);
+            this.chkUseSymb.Name = "chkUseSymb";
+            this.chkUseSymb.Size = new System.Drawing.Size(142, 24);
+            this.chkUseSymb.TabIndex = 11;
+            this.chkUseSymb.Text = "Use Symbols";
+            this.chkUseSymb.UseVisualStyleBackColor = true;
+            // 
+            // lblPassLen
+            // 
+            this.lblPassLen.AutoSize = true;
+            this.lblPassLen.Location = new System.Drawing.Point(459, 44);
+            this.lblPassLen.Name = "lblPassLen";
+            this.lblPassLen.Size = new System.Drawing.Size(76, 20);
+            this.lblPassLen.TabIndex = 12;
+            this.lblPassLen.Text = "Length:";
+            // 
+            // txtPassLen
+            // 
+            this.txtPassLen.BackColor = System.Drawing.SystemColors.Window;
+            this.txtPassLen.Location = new System.Drawing.Point(541, 40);
+            this.txtPassLen.MaxLength = 2;
+            this.txtPassLen.Name = "txtPassLen";
+            this.txtPassLen.Size = new System.Drawing.Size(35, 27);
+            this.txtPassLen.TabIndex = 13;
+            this.txtPassLen.Text = "10";
+            this.txtPassLen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPassLen.WordWrap = false;
+            this.txtPassLen.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPassLen_KeyDown);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 301);
+            this.Controls.Add(this.txtPassLen);
+            this.Controls.Add(this.lblPassLen);
+            this.Controls.Add(this.chkUseSymb);
+            this.Controls.Add(this.txtNumFirst);
+            this.Controls.Add(this.lblNumFirst);
+            this.Controls.Add(this.chkUseFirst);
+            this.Controls.Add(this.lblBase);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.txtPass);
+            this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XCrypt Secure";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private TextBox txtPass;
+		private Label lblPass;
+		private Button btnGen;
+		private TextBox txtBase;
+		private Label lblBase;
+		private CheckBox chkUseFirst;
+		private Label lblNumFirst;
+		private TextBox txtNumFirst;
+		private CheckBox chkUseSymb;
+		private Label lblPassLen;
+		private TextBox txtPassLen;
+	}
+}
+
---
+++ PassGen1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace XCrypt {
+	public partial class frmMain : Form {
+
+		bool hasRanOnce = false;
+		int minLen = 10;
+
+		public frmMain() => InitializeComponent();
+
+		private void btnGen_Click(object sender, EventArgs e) {
+			string strBase = txtBase.Text;
+			bool useFirst = chkUseFirst.Checked;
+			bool useSymbols = chkUseSymb.Checked;
+			int numFirst = CInt(txtNumFirst.Text);
+			int passLen = CInt(txtPassLen.Text);
+			if (passLen < minLen) {
+				txtPassLen.Text = CStr(minLen);
+				passLen = minLen;
+			}
+			txtPass.Text = generatePassword(strBase, passLen, useSymbols, useFirst, numFirst);
+		}
+
+		private string[] getLetters() => new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i",
+								  "j", "k", "l", "m", "n", "o", "p", "q", "r",
+								  "s", "t", "u", "v", "w", "x", "y", "z"};
+
+		private string[] getSymbols() => new string[] {"!", "@", "#", "$", "%", "^", "&", "*",
+								 "(",")","{","}","`", "~", ",", "[", "]",
+								 ".", "<", ">", ":", ";", "=", "+", "-",
+								 "/", "?", "\"", "'", "|", "_"};
+
+		private int[] getNumbers() => new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		private string generatePassword(string strBase = "", int passLen = 13, bool useSymbols = true, bool useFirst = true, int firstNum = 2) {
+			string strPass = "";
+			strBase = (strBase == "") ? "xcryptpw" : strBase;
+			char[] strPw = strBase.ToCharArray();
+			if (useFirst) {
+				int b = 0;
+				while (b < firstNum) {
+					string strFirst = strPw[b].ToString();
+					if (b == 0) {
+						char[] strSubPass = strFirst.ToCharArray();
+						strPass += strSubPass[0].ToString().ToUpper();
+						b = 1;
+					}
+					strPass += strPw[b].ToString();
+					b++;
+				}
+			}
+			int i = 0;
+			int ns = 0;
+			Random rand = new Random();
+			while (strPass.Length < passLen) {
+				int x = rand.Next(0, strPw.Length - 1);
+				int y = rand.Next(0, 2);
+				string[] ltrs = getLetters();
+				if (i == rand.Next(5, passLen)) {
+					strPass += rand.Next(strPw.Length);
+				}
+				strPass += ltrs[rand.Next(0, 25)];
+				int h = rand.Next(0, 25);
+				strPass += (y == 0) ? ltrs[h].ToUpper() : ltrs[h].ToLower();
+				int[] nums = getNumbers();
+				if (useSymbols) {
+					string[] symbs = getSymbols();
+					int symbLen = symbs.Length - 1;
+					string u = symbs[rand.Next(0, symbLen)];
+					if (u != "" && ns < rand.Next(2, 6)) {
+						strPass += u;
+						ns++;
+					} else {
+						if (rand.Next(0, 10) < 5) {
+							strPass += nums[rand.Next(0, 9)];
+						} else {
+							strPass += ltrs[rand.Next(0, 9)];
+						}
+					}
+					strPass += nums[rand.Next(0, 9)];
+				}
+				i++;
+			}
+			strPass = strPass.Replace(" ", "");
+			if (!hasRanOnce) {
+				hasRanOnce = true;
+				strPass = generatePassword(strPass, passLen, useSymbols, useFirst, firstNum);
+			}
+			strPass = strPass.Substring(0, passLen);
+			return strPass;
+		}
+
+		private void txtNumFirst_KeyDown(object sender, KeyEventArgs e) {
+			string[] keyNums = { CStr(Keys.D0), CStr(Keys.D1), CStr(Keys.D2), CStr(Keys.D3), CStr(Keys.Back) };
+			if (!(Array.Exists(keyNums, el => el == CStr(e.KeyCode)))) {
+				e.SuppressKeyPress = true;
+			}
+		}
+
+		private void txtPassLen_KeyDown(object sender, KeyEventArgs e) {
+			string[] keyNums = { CStr(Keys.D0), CStr(Keys.D1), CStr(Keys.D2), CStr(Keys.D3), CStr(Keys.D4),
+								 CStr(Keys.D5), CStr(Keys.D6), CStr(Keys.D7), CStr(Keys.D8), CStr(Keys.D9),
+								 CStr(Keys.Back) };
+			if (!(Array.Exists(keyNums, el => el == CStr(e.KeyCode)))) {
+				e.SuppressKeyPress = true;
+			}
+		}
+
+		private string CStr(object exp) => Convert.ToString(exp);
+		private int CInt(object exp) => Convert.ToInt32(exp);
+	}
+}
